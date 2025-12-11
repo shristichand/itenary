ac0cd8e6d8dc8357bc308c49918b9af77cab1ca2
krezonatechnology <shristichand658@gmail.com>
2025-12-11 10:21:03 +0545

    testimonial

--- frontend/src/components/testimonial/Testimonial.tsx
+++ frontend/src/components/testimonial/Testimonial.tsx
@@ -10,21 +10,21 @@ import { useQuery } from "@tanstack/react-query";
 export const Testimonial = () => {
     const { data: reviewsData, isLoading } = useQuery({
         queryKey: ["reviews"],
-        queryFn: () => getReviews()
+        queryFn: () => getReviews(),
+        select: (res) => res.data
     });
     if (isLoading) {
         return <div>Loading...</div>
     }
 
 
-    const reviews = reviewsData?.data || [];
     return (
         <section>
             <MaxWidthWrapper>
                 <div className="space-y-5 py-10 ">
                     <SubHeadingContainer headingtext="What Our Travelers Say" paragraphtext="Real experiences from real travelers who have explored the world with us." />
                     <div className="flex md:flex-wrap flex-nowrap md:justify-center overflow-x-auto md:overflow-visible gap-4 md:gap-x-7.5 md:gap-y-10 snap-x snap-mandatory md:px-0 pb-4 md:pb-0 scrollbar-hide">
-                        {reviews.map((item: any, index: number) => (
+                        {reviewsData?.map((item: any, index: number) => (
                             <div key={index} className="snap-center flex-shrink-0">
                                 <Card testimonial={item.Review} star={item.Rating} image={item.image} name={item.Name} location={item.Location} />
                             </div>
